bb9c8f72e38b6687982e90f7393b1c85630367d3
Rahul <rahulhs0699@gmail.com>
2022-10-15 23:48:54 +0530

    Changes in Profile Page

--- frontend/expense-app/src/components/UserProfile.js
+++ frontend/expense-app/src/components/UserProfile.js
@@ -1,17 +1,19 @@
 import React,{useState} from 'react'
 import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { startAddProfilePicture } from '../actions/userAction' 
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { startAddProfilePicture,startAddName } from '../actions/userAction' 
 import { useDispatch,useSelector } from 'react-redux'
 
 const UserProfile=(props)=>{
     
     const token=JSON.parse(localStorage.getItem('jwtToken'))
-    const profilePic=useSelector(state=>state.profilePic)
-      
+    const userInfo=useSelector(state=>state.userInfo)
+    console.log('profile pic in comp',userInfo)  
     const dispatch=useDispatch()
-    const [image,setImage]=useState()
-    const [name,setName]=useState('')
+    const [image,setImage]=useState(userInfo.hasOwnProperty('picture')?userInfo.picture:null)
+    const [name,setName]=useState(userInfo.hasOwnProperty('name')?userInfo.name:null)
+    const [toggle,setToggle]=useState(false)
     console.log('name',name)
     const handleFileUpload=(e)=>{
         const type=e.target.name
@@ -23,8 +25,9 @@ const UserProfile=(props)=>{
         setName(e.target.value)
     }
 
-    const handleSubmit=()=>{
-
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        dispatch(startAddName({name:name},token))
     }
     const handleButtonClick=()=>{
         const data=new FormData()
@@ -32,31 +35,40 @@ const UserProfile=(props)=>{
 
         console.log('image data',data)
         dispatch(startAddProfilePicture(data,token)) 
+        
   }
 
     return (
         <div>
             <center><h1>User Profile</h1>
-                {Object.keys(profilePic).length!=0?<img src='' />:
+            <div style={{border:'solid 1px black', padding:'40px'}}>
+               <div> {(name||image)&&<i className="bi bi-pencil-square" ></i>}</div>
+                {userInfo.hasOwnProperty('picture')?<><img src={`http://localhost:3050/images/${userInfo.picture.slice(7)}`} /><br /><br /></>:
                     <div>
                         <form >
                     
-                    <label>Please upload a profile picture</label><br />
                     <div className="mb-3">
                         
-                        <input className="form-control" type="file" id="formFile" onChange={handleFileUpload} name='image' />
-                    </div>
+                    <input className="form-control" type="file" id="formFile" onChange={handleFileUpload} name='image' />
+                </div>
+                    <label>Please upload a profile picture</label><br />
+                    
                     
                   </form>
                   <button className='btn btn-primary' onClick={handleButtonClick}>Send</button><br /><br />
                     </div>
                 }
-                <form onSubmit={handleSubmit}>
-                    <input className='form-control'type='text' value={name} name='name' placeholder='Set a profile name' onChange={handleChange} /><br />
-                    <input type='submit' value='OK'/>
-                </form>
+                {userInfo.hasOwnProperty('name')?<>
+                         <h1>Hola,{userInfo.name}!</h1>
+                         
+                    </>:
+                 <form onSubmit={handleSubmit}>
+                 <input className='form-control'type='text' value={name} name='name' placeholder='Set a profile name' onChange={handleChange} /><br />
+                 <input type='submit' value='OK'/>
+             </form>}
+               
 
-                
+             </div>
             </center>
 
         </div>
